3b1b7a19162bc7ee7ef4b9adde6871a67e9fb180
Meli-Melow <lyzmeliamomo11@gmail.com>
2024-07-30 15:04:28 +0200

    Update UtilisateurMapper.java

--- teamV3/src/main/java/v3/team/mapper/Interfaces/UtilisateurMapper.java
+++ teamV3/src/main/java/v3/team/mapper/Interfaces/UtilisateurMapper.java
@@ -11,16 +11,10 @@ public interface UtilisateurMapper {
     @Mapping(source="prenom", target = "prenom")
     @Mapping(source="nom", target = "nom")
     @Mapping(source = "email",target = "email")
-/*
-    @Mapping(source="questionsCertifiees", target = "questionsCertifiees")
-*/
     UtilisateurDto toDto(Utilisateur utilisateur);
 
     @Mapping(source="prenom", target = "prenom")
     @Mapping(source="nom", target = "nom")
     @Mapping(source = "email",target = "email")
-/*
-    @Mapping(source="questionsCertifiees", target = "questionsCertifiees")
-*/
     Utilisateur toClasse (UtilisateurDto utilisateurDto);
 }
